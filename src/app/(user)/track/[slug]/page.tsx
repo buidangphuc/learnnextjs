aff60b148a55682214d5743e2adcc06e98918fdf
Bùi Đăng phúc <95173128+buidangphuc@users.noreply.github.com>
2025-01-17 14:40:23 +0700

    add loading middleware

--- src/app/(user)/track/[slug]/page.tsx
+++ src/app/(user)/track/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import WaveTrack from '@/components/track/wave.track';
 import Container from '@mui/material/Container';
 import { sendRequest } from '@/utils/api';
+import { notFound } from 'next/navigation'
 
 import type { Metadata, ResolvingMetadata } from 'next'
 
@@ -60,6 +61,11 @@ const DetailTrackPage = async (props: any) => {
             sort: "-createdAt"
         },
     })
+    await new Promise(resolve => setTimeout(resolve, 3000))
+
+    if (!res?.data)
+
+        notFound()
 
     return (
         <Container>
